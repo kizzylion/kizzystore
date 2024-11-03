82b46c9c4db1e6ec8b5a59822e26083da0595acb
kizzylion <kztchm@gmail.com>
2024-11-03 11:16:09 -0500

    FIX: GOOGLE ANALYTICS MOVED TO ROOT

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./tailwind.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import ReactGA from "react-ga";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-  useLocation,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/Root";
 import ErrorPage from "./ErrorElement";
 import LandingPage from "./pages/landingPage/LandingPage";
 
 // Initialize Google Analytics using the tracking ID from the .env file
-ReactGA.initialize(import.meta.env.VITE_GA_TRACKING_ID);
 
 const routes = createBrowserRouter([
   {
@@ -32,19 +26,9 @@ const routes = createBrowserRouter([
 ]);
 
 // Track page views when the route changes
-function Analytics() {
-  const location = useLocation();
-
-  useEffect(() => {
-    ReactGA.pageview(location.pathname + location.search);
-  }, [location]);
-
-  return null;
-}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Analytics />
     <RouterProvider router={routes} />
   </StrictMode>,
 );
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,10 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Heading from "../components/Heading";
+import ReactGA from "react-ga";
+import { useEffect } from "react";
+
+ReactGA.initialize(import.meta.env.VITE_GA_TRACKING_ID);
 
 const Root = () => {
+  function Analytics() {
+    const location = useLocation();
+
+    useEffect(() => {
+      ReactGA.pageview(location.pathname + location.search);
+    }, [location]);
+
+    return null;
+  }
+
   return (
     <>
       <Heading />
+      <Analytics />
       <main>
         <Outlet />
       </main>
